f639fc81e2299e59aac282ba5c3d3e9aff62c949
felixienny <felix.ienny@utt.fr>
2020-11-21 20:25:35 +0100

    PlayerH add the method askMove

--- projet_ShapUp_LO02/src/projet_ShapUp_LO02/PlayerH.java
+++ projet_ShapUp_LO02/src/projet_ShapUp_LO02/PlayerH.java
@@ -1,10 +1,60 @@
-package projet_ShapUp_LO02;
+package projet_ShapUp_LO02.src.projet_ShapUp_LO02;
+import java.util.Scanner;
 
 public class PlayerH extends Player {
 	public PlayerH(String name) {super(name);}
 	
-	public void askMove(Card Card) {
-		
+	public void askMove(Card card) {
+		// this.display()
+		Scanner scannerChoix = new Scanner(System.in);
+		int choix;
+		do {
+			System.out.println("Que veux-tu faire :");
+			System.out.println("1 - Jouer la carte que tu as pioché");
+			System.out.println("2 - Déplacer une carte déjà sur le jeu");
+			choix = scannerChoix.nextInt();
+		} while (!(choix==1 || choix==2));
+		if (choix==1) {
+			int x;
+			int y;
+			do {
+				System.out.println("Où veux-tu la jouer ?");
+				String choixPlace = scannerChoix.next().trim();
+				
+				while (!choixPlace.matches("^[0-9],[0-9]$")) {
+					System.out.println("Mauvaise saisie, où veux-tu la jouer ?");
+					System.out.println("Exemple : 1,7");
+					choixPlace = scannerChoix.next().trim();
+				}
+				x = Integer.valueOf(choixPlace.split(",")[0]);
+				y = Integer.valueOf(choixPlace.split(",")[1]);
+			} while (this.playingGridAdress.isFreeToPlaceACardOn(x, y));
+			this.playingGridAdress.setTile(x, y, card);
+		}
+		else if (choix==2) {
+			// this.display();
+			int xSrc;
+			int ySrc;
+			int xDest;
+			int yDest;
+			do { 
+				System.out.println("Quelle carte veux-tu déplacer ?");
+				String choixMove = scannerChoix.next().trim();
+				
+				while (!choixMove.matches("^[0-9],[0-9]:[0-9],[0-9]$")) {
+					System.out.println("Mauvaise saisie, quel carte veux-tu déplacer ?");
+					System.out.println("Exemple : 1,3:6,7");
+					choixMove = scannerChoix.next().trim();
+				}
+				xSrc = Integer.valueOf(choixMove.split(":")[0].split(",")[0]);
+				ySrc = Integer.valueOf(choixMove.split(":")[0].split(",")[1]);
+				xDest = Integer.valueOf(choixMove.split(":")[1].split(",")[1]);
+				yDest = Integer.valueOf(choixMove.split(":")[1].split(",")[1]);
+			} while(!this.playingGridAdress.isFreeToPlaceACardOn(xSrc, ySrc) && this.playingGridAdress.isFreeToPlaceACardOn(xDest, yDest));
+			this.playingGridAdress.setTile(xDest, yDest, this.playingGridAdress.getTile(xSrc, ySrc));
+			// TODO Remove the card placed before
+		}
+		scannerChoix.close();
 	}
 	
 	public void display() {
