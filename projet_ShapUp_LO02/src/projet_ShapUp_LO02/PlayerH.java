d6585e6299a1254e81b1196b3670a13663f3a80a
felixienny <felix.ienny@utt.fr>
2020-11-21 12:08:25 +0100

    tournoi et playerH

--- projet_ShapUp_LO02/src/projet_ShapUp_LO02/PlayerH.java
+++ projet_ShapUp_LO02/src/projet_ShapUp_LO02/PlayerH.java
@@ -1,14 +1,15 @@
 package projet_ShapUp_LO02;
+
 public class PlayerH extends Player {
 	public PlayerH(String name)
 	{
 		super(name);
 	}
 	
-	public void askMove(Card victoryCard)
-	{
+	public void askMove(Card Card) {
 		
 	}
+	
 	public void display() {
 		int width = this.playingGridAdress.getWidth();
 		int height = this.playingGridAdress.getHeight();
@@ -70,7 +71,65 @@ public class PlayerH extends Player {
 		System.out.println(ANSI_RESET);
 	}
 	
-	public static void main(String args[]) {
-		
-	}
+	// public static void main(String args[]) {
+	// 	int width = 20; //this.playingGridAdress.getWidth();
+	// 	int height = 5;
+	// 	Deck d = new Deck(178);
+	// 	String ANSI_BLACK  = "\u001B[30m";
+	// 	String ANSI_RESET  = "\u001B[0m";
+	// 	String ANSI_BG_RED    = "\u001B[41m";
+	// 	String ANSI_BG_GREEN  = "\u001B[42m";
+	// 	String ANSI_BG_BLUE   = "\u001B[44m";
+	// 	for(int h=0; h<=height; h++) {
+	// 		for(int w=0; w<=width; w++) {
+	// 			System.out.print(" ");
+	// 			// if(!this.playingGridAdress.isFreeToPlaceACardOn(w, h)) {
+	// 				Card card = d.pickNextCard();
+	// 				if(card.getHollow()) {
+	// 					System.out.print(ANSI_BLACK);
+	// 				}
+	// 				else {						
+	// 					System.out.print(ANSI_RESET);
+	// 				}
+
+	// 				switch (card.getColor()) {
+	// 				case RED:
+	// 					System.out.print(ANSI_BG_RED);
+	// 					break;
+	// 				case GREEN:
+	// 					System.out.print(ANSI_BG_GREEN);
+	// 					break;
+	// 				case BLUE:
+	// 					System.out.print(ANSI_BG_BLUE);
+	// 					break;
+	// 				default:
+	// 					break;
+	// 				}
+
+	// 				switch (card.getShape()) {
+	// 				case CIRCLE:
+	// 					System.out.print(" • ");
+	// 					break;
+	// 				case TRIANGLE:
+	// 					System.out.print(" ▲ ");
+	// 					break;
+	// 				case SQUARE:
+	// 					System.out.print(" ■ ");
+	// 					break;
+	// 				default:
+	// 					break;
+	// 				}
+					
+	// 			// }
+	// 			// else{
+	// 			// 	System.out.print("   ");
+	// 			// }
+
+	// 			System.out.print(ANSI_RESET + " ");
+	// 		}		  
+	// 		System.out.println("\n");
+	// 	}
+	// 	System.out.println(ANSI_RESET);
+	// }
+	// }
 }
